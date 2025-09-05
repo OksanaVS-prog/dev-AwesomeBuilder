796b3ce4c9540eda56068516a1092ba0fa7257cc
OksanaVS-prog <kramarenkoksana@ukr.net>
2025-09-05 23:05:46 +0300

    create position-sticky and mob version

--- src/main.js
+++ src/main.js
@@ -22,3 +22,18 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter'))
+
+const getLoremImage = () => {
+    const images = document.querySelectorAll('img');
+    
+    images.forEach(item => {
+        if (!item.getAttribute('src')) {
+            fetch('https://picsum.photos/640/360')
+                .then(response => response.blob())
+                .then(blob => item.src = URL.createObjectURL(blob))
+                .catch(error => item.src = "assets/img/cat-scottish.webp");
+        }
+    });
+};
+
+document.querySelectorAll('img').length > 0 ? getLoremImage() : null;
