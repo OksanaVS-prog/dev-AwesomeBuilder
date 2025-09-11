6458203c4f4b81017e4bd39b8b9e2e72f901ef91
OksanaVS-prog <kramarenkoksana@ukr.net>
2025-09-11 19:24:11 +0300

    create adaptation to mob version

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 
 const burgerBtn = document.querySelector('#burger');
 const mobileContainer = document.querySelector('#mobile-container')
+const body = document.querySelector('body')
 
 burgerBtn.addEventListener('click', ()=> {
   mobileContainer.classList.toggle('hidden')
   burgerBtn.classList.toggle('before:bg-purple')
   burgerBtn.classList.toggle('after:bg-purple')
+  body.classList.toggle('overflow-hidden')
 });
 
 
