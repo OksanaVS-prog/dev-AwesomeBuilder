22e39f04494fb6c38309a0c27ebe9faf1ecb8f7d
OksanaVS-prog <kramarenkoksana@ukr.net>
2025-09-25 19:31:16 +0300

    partners-slide done and adaptation to mob version

--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
 
+const swiper = new Swiper('.swiper', {
+  spaceBetween: 20,
+
+  breakpoints: {
+    0: { // мобильные
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      grid: {
+        rows: 3,
+        fill: 'row',
+      },
+      loop: false, // ⚠️ обязательно выключить!
+    },
+    1024: { // десктоп
+      slidesPerView: 4,
+      slidesPerGroup: 1,
+      loop: true,
+      grid: {
+        rows: 1, // иначе loop ломается
+      },
+    },
+  },
+});
+
+const { log } = require("console");
+
 const burgerBtn = document.querySelector('#burger');
 const mobileContainer = document.querySelector('#mobile-container')
 const body = document.querySelector('body')
