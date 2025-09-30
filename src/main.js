4cbc55d923f8ca583363913163580edf175da591
OksanaVS-prog <kramarenkoksana@ukr.net>
2025-09-30 11:53:30 +0300

    adaptation tabs to mob version

--- src/main.js
+++ src/main.js
@@ -23,8 +23,6 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-const { log } = require("console");
-
 const burgerBtn = document.querySelector('#burger');
 const mobileContainer = document.querySelector('#mobile-container')
 const body = document.querySelector('body')
@@ -36,5 +34,46 @@ burgerBtn.addEventListener('click', ()=> {
   body.classList.toggle('overflow-hidden')
 });
 
+  const installTabs = ()=> {
+  const tabsButtons = document.querySelectorAll('[data-tab-content]')
+  const tabContent = document.querySelectorAll('.tab-content')
+
+  const removeActiveClassForNav =()=> {
+    tabsButtons.forEach(item=> item.classList.remove('bg-purple'))
+  }
+
+  const hiddenContentTabs =()=> {
+    tabContent.forEach(item=> item.classList.add('hidden'))
+  }
 
 
+  tabsButtons.forEach((item, index, array)=>{
+    const tabsButton = item.getAttribute('data-tab-content');
+
+    item.addEventListener('click', ()=> {
+      const showContent = document.getElementById(tabsButton)
+      removeActiveClassForNav()
+      item.classList.add('bg-purple')
+
+      hiddenContentTabs()
+      showContent.classList.remove('hidden')
+    })
+  })
+}
+
+document.querySelectorAll('.tabs').length ? installTabs() : null;
+
+const getLoremImage = () => {
+        const images = document.querySelectorAll('img');
+
+        images.forEach(item => {
+            if (!item.getAttribute('src')) {
+                fetch('https://picsum.photos/332/420')
+                    .then(response => response.blob())
+                    .then(blob => item.src = URL.createObjectURL(blob))
+                    .catch(error => item.src = "assets/img/cat-scottish.webp");
+            }
+        });
+    };
+
+ document.querySelectorAll('img').length ? getLoremImage() : null;
